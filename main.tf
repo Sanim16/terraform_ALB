a782199566af55c70b0dfe5673e860c9e8bd44bd
Momoh Sani Musa <sanim16@yahoo.com>
2024-10-12 01:53:44 +0100

    add security_group.tf

--- main.tf
+++ main.tf
@@ -95,30 +95,3 @@ resource "aws_security_group" "instances" {
     Name = "terraform-${var.vpc_name}-instances"
   }
 }
-
-resource "aws_vpc_security_group_ingress_rule" "instances-ssh" {
-  security_group_id = aws_security_group.instances.id
-
-  cidr_ipv4   = "0.0.0.0/0"
-  from_port   = 22
-  ip_protocol = "tcp"
-  to_port     = 22
-}
-
-resource "aws_vpc_security_group_ingress_rule" "instances-http" {
-  security_group_id = aws_security_group.instances.id
-
-  referenced_security_group_id = aws_security_group.alb.id
-  from_port                    = 80
-  ip_protocol                  = "tcp"
-  to_port                      = 80
-}
-
-resource "aws_vpc_security_group_ingress_rule" "instances-https" {
-  security_group_id = aws_security_group.instances.id
-
-  cidr_ipv4   = "0.0.0.0/0"
-  from_port   = 443
-  ip_protocol = "tcp"
-  to_port     = 443
-}
---
+++ security_group.tf
@@ -0,0 +1,61 @@
+resource "aws_security_group" "alb" {
+  name        = "terraform-${var.vpc_name}-alb"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = module.vpc.vpc_id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "terraform-${var.vpc_name}-alb"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "alb-http" {
+  security_group_id = aws_security_group.alb.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 80
+  ip_protocol = "tcp"
+  to_port     = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "alb-https" {
+  security_group_id = aws_security_group.alb.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 443
+  ip_protocol = "tcp"
+  to_port     = 443
+}
+
+resource "aws_vpc_security_group_ingress_rule" "instances-ssh" {
+  security_group_id = aws_security_group.instances.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 22
+  ip_protocol = "tcp"
+  to_port     = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "instances-http" {
+  security_group_id = aws_security_group.instances.id
+
+  referenced_security_group_id = aws_security_group.alb.id
+  from_port                    = 80
+  ip_protocol                  = "tcp"
+  to_port                      = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "instances-https" {
+  security_group_id = aws_security_group.instances.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 443
+  ip_protocol = "tcp"
+  to_port     = 443
+}
